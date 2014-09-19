d9f70c69638aac694d9ecff788092902222fdbe0
Jerome Pratt <jerome.pratt@outlook.com>
2014-09-19 14:01:54 +0100

    Adding test for knowing when not to yo, and re-refactoring

--- spec/TimeCheckerSpec.js
+++ spec/TimeCheckerSpec.js
@@ -7,18 +7,23 @@ describe("TimeChecker", function() {
 
   withTimeFrozenAt("2014-01-01 09:30:00 GMT+0000", function(){
 
-    it("knows when the time is 9:30am", function(){
+    it("knows it's time to yo when it's 9:30am", function(){
       expect(timeChecker.yoTime()).toBe(true)
     })
   })
 
   withTimeFrozenAt("2014-01-01 14:30:00 GMT+0000", function(){
 
-    it("knows when the time is 2:30pm", function(){
+    it("knows it's time to yo when it's 2:30pm", function(){
       expect(timeChecker.yoTime()).toBe(true)
     })
   })
 
+  withTimeFrozenAt("2014-01-01 9:00:00 GMT+0000", function(){
 
+    it("knows it's not time to yo when it's 9am", function(){
+      expect(timeChecker.yoTime()).toBe(false)
+    })
+  })
 
 });
--- src/TimeChecker.js
+++ src/TimeChecker.js
@@ -9,6 +9,10 @@ TimeChecker.prototype.yoTime = function() {
 	var date = new Date
 	var hour = date.getHours()
 	var minute = date.getMinutes()
-	if((hour + ":" + minute) == "9:30" || "14:30") return(true)
-	
+	if((hour + ":" + minute) == "9:30" || (hour + ":" + minute) == "14:30") return(true)
+	else return(false)
+};
+
+TimeChecker.prototype.timeFormat = function() {
+	hour + ":" + minute
 };
