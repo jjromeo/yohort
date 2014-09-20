68895fbcf52488afb99cabfc8d00431f5e0aa717
jjromeo <jerome.pratt@outlook.com>
2014-09-20 17:02:45 +0100

    Will now only send yo's during the week, also refactored YoTimeChecker

--- spec/YoTimeCheckerSpec.js
+++ spec/YoTimeCheckerSpec.js
@@ -5,25 +5,73 @@ describe("YoTimeChecker", function() {
     timeChecker = new YoTimeChecker
   });
 
-  withTimeFrozenAt("2014-01-01 09:25:00 GMT+0000", function(){
+  describe("Time of day to yo:", function(){
 
-    it("knows it's time to yo when it's 9:25am", function(){
-      expect(timeChecker.yoTime()).toBe(true)
+    withTimeFrozenAt("2014-01-01 09:25:00 GMT+0000", function(){
+
+      it("knows it's time to yo when it's 9:25am", function(){
+        expect(timeChecker.isYoTime()).toBe(true)
+      })
     })
+
+    withTimeFrozenAt("2014-01-01 14:25:00 GMT+0000", function(){
+
+      it("knows it's time to yo when it's 2:25pm", function(){
+        expect(timeChecker.isYoTime()).toBe(true)
+      })
+    })
+
+    withTimeFrozenAt("2014-01-01 9:00:00 GMT+0000", function(){
+
+      it("knows it's not time to yo when it's 9am", function(){
+        expect(timeChecker.isYoTime()).toBe(false)
+      })
+    })
+
   })
 
-  withTimeFrozenAt("2014-01-01 14:25:00 GMT+0000", function(){
+  describe("Weekend behaviour:", function(){
 
-    it("knows it's time to yo when it's 2:25pm", function(){
-      expect(timeChecker.yoTime()).toBe(true)
+    withTimeFrozenAt("2014-01-01 09:25:00 GMT+0000", function(){
+
+      it("knows when it's not the weekend", function(){
+        expect(timeChecker.isWeekend()).toBe(false)
+      })
+
     })
+
+    withTimeFrozenAt("2014-01-04 09:25:00 GMT+0000", function(){
+
+      it("knows Saturday is on the weekend", function(){
+        expect(timeChecker.isWeekend()).toBe(true)
+      })
+    })
+
+
+    withTimeFrozenAt("2014-01-05 09:25:00 GMT+0000", function(){
+
+      it("knows Sunday is on the weekend", function(){
+        expect(timeChecker.isWeekend()).toBe(true)
+      })
+    })
+
+    withTimeFrozenAt("2014-01-04 09:25:00 GMT+0000", function(){
+
+      it("will not yo on a Saturday", function(){
+        expect(timeChecker.isYoTime()).toBe(false)
+      })
+
+    })
+
+    withTimeFrozenAt("2014-01-05 09:25:00 GMT+0000", function(){
+
+      it("will not yo on a Sunday", function(){
+        expect(timeChecker.isYoTime()).toBe(false)
+      })
+
+    })
+
   })
 
-  withTimeFrozenAt("2014-01-01 9:00:00 GMT+0000", function(){
-
-    it("knows it's not time to yo when it's 9am", function(){
-      expect(timeChecker.yoTime()).toBe(false)
-    })
-  })
 
 });
--- src/YoTimeChecker.js
+++ src/YoTimeChecker.js
@@ -5,11 +5,26 @@ var minute;
 function YoTimeChecker(){
 }
 
-YoTimeChecker.prototype.yoTime = function() {
-	var date = new Date
-	var hour = date.getHours()
-	var minute = date.getMinutes()
-	if((hour + ":" + minute ) == "9:25" || (hour + ":" + minute ) == "14:25" ) return(true)
+YoTimeChecker.prototype.isYoTime = function() {
+ if(this.isRightTime() && (!this.isWeekend())) return(true)
 	else return(false)
 };
 
+YoTimeChecker.prototype.isWeekend = function() {
+	var day = new Date().getDay()
+	if (day == 6 || day == 0){
+		return(true)
+	}
+	else{
+		return(false)
+	}
+};
+
+YoTimeChecker.prototype.isRightTime = function() {
+	var date = new Date
+	var hour = date.getHours()
+	var minute = date.getMinutes()
+	if((hour + ":" + minute ) == "9:25" || (hour + ":" + minute ) == "14:25"){
+		return(true)
+	}
+};
