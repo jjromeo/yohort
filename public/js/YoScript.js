c076c7f423e9fa1231e9d8c293bc8eb3b599a441
Jerome Pratt <jerome.pratt@outlook.com>
2014-09-29 19:44:58 +0100

    fixing api_token to environmental variable

--- public/js/YoScript.js
+++ public/js/YoScript.js
@@ -6,7 +6,7 @@ setInterval(function(){
 
 	if (timeChecker.isYoTime()){
 			setTimeout(function(){
-		$.post('http://api.justyo.co/yoall/', {'api_token': myToken,
+		$.post('http://api.justyo.co/yoall/', {'api_token': ENV['YOHORT_API_KEY'],
 		// 'link': 'http://optional-link.com'
 		}, function(response){}), 1000
 		});
